9cc4259236d8bb5844d10aa6db49f9d2fa301fc4
Macbook Pro <macbookpro@MBP-cua-Macbook.lan>
2024-03-01 12:50:36 +0700

    mes1

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./App.css";
+function App() {
+  const [count, setCount] = useState(1);
+  let color = count > 0 ? "green" : count < 0 ? "red" : "blue";
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const resetcount = () => setCount(0);
+
+  return (
+    <div className="container">
+      <h1>Counter</h1>
+      <span style={{ color }} id="value">
+        {count}
+      </span>
+      <div className="btn-group">
+        <button onClick={decrement} className="btn btn-decrease">
+          DECREASE
+        </button>
+        <button onClick={resetcount} className="btn btn-reset">
+          RESET
+        </button>
+        <button onClick={increment} className="btn btn-increase">
+          INCREASE
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
